6861ff772f7624ef5a176121a90df9f64047b5b2
Lukas Hennig <lukas@wirsindwir.de>
2016-12-12 18:50:37 +0100

    Initial commit.

---
+++ src/main/java/de/mais/elytragame/ElytraGame.java
@@ -0,0 +1,16 @@
+package de.mais.elytragame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElytraGame extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
